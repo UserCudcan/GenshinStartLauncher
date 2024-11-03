d1aea50c58d01a71fe3d928abaf58f2d78df5f87
UserCudcan <youis123321@gmail.com>
2024-11-03 12:17:36 +0800

    1.0.3alpha

---
+++ 2.bat
@@ -0,0 +1,6 @@
+@echo off
+start GI.lnk
+echo 原神启动
+echo 启动的是BETA1.4.50测试版
+pause
+
---
+++ 3.bat
@@ -0,0 +1,10 @@
+@echo off
+echo 作者:Cudcan
+echo 把网络关了，避免MHY日志
+echo 如果你点了测试角色，不能加载，请用windows创建一个不同的新用户再打开
+echo 某个游戏=MHY GI
+
+echo 1=打开必读并打开某个游戏
+echo 2=不打开必读并打开某个游戏
+echo 3=退出
+pause
